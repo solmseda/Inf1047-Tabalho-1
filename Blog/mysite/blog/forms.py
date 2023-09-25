8e9f3be0f330dd7837481f505de3df449cca5555
Rafael <rafaelpmeirelles@gmail.com>
2023-09-25 15:34:57 -0300

    Primeira iteração tela de registro

--- Blog/mysite/blog/forms.py
+++ Blog/mysite/blog/forms.py
@@ -10,5 +10,5 @@ class LoginForm(forms.Form):
 class RegisterForm(UserCreationForm):
     class Meta:
         model=User
-        fields = ['username','email','password1','password2'] 
+        fields = ['username','password1','password2'] 
    
