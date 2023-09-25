2aa4097f60f4e9f312a52e88f098ae6d8a81cce3
Sol <solmseda@gmail.com>
2023-09-25 17:40:30 -0300

    Modelo de post
    Iniciado o modelo de uma postagem com titulo, corpo, slug, data de publicação, data de criação, data de atualização e status (draft e publicado)

--- Blog/mysite/blog/models.py
+++ Blog/mysite/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Post(models.Model):
 
@@ -7,17 +8,18 @@ class Post(models.Model):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
 
-    title = models.CharField(max_length=250)
+    titulo = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
-    body = models.TextField()
-    publish = models.DateTimeField(default=timezone.now)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    #autor = models.ForeignKey(User, on_delete = models.CASCADE, related_name='blog_posts')
+    corpo = models.TextField()
+    dt_publicado = models.DateTimeField(default=timezone.now)
+    dt_criado = models.DateTimeField(auto_now_add=True)
+    dt_atualizado = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
 
     class Meta:
-        ordering = ['-publish']
-        indexes = [models.Index(fields=['-publish'])]
+        ordering = ['-dt_publicado']
+        indexes = [models.Index(fields=['-dt_publicado'])]
 
     def __str__(self):
-        return self.title
+        return self.titulo
