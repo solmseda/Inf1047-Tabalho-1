cb83aee3ffb4df612bba4dc5b06ab0da3512ad48
Rafael <rafaelpmeirelles@gmail.com>
2023-10-02 18:35:35 -0300

    Comecando a mexer com o css

--- Blog/mysite/blog/forms.py
+++ Blog/mysite/blog/forms.py
@@ -4,16 +4,30 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Post
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=65)
-    password = forms.CharField(max_length=65, widget=forms.PasswordInput)
-    
+    username = forms.CharField(
+        max_length=65,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'})
+    )
+    password = forms.CharField(
+        max_length=65,
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'})
+    )
 
 class RegisterForm(UserCreationForm):
     class Meta:
-        model=User
-        fields = ['username','password1','password2'] 
-   
+        model = User
+        fields = ['username', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'})
+        }
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['titulo', 'corpo']
+        widgets = {
+            'titulo': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title'}),
+            'corpo': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Body'})
+        }
