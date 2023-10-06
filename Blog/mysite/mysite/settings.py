cbc400f9d12978cddd229ee9706b906f03963476
Sol <solmseda@gmail.com>
2023-10-06 15:55:33 -0300

    arquivos para o heroku

--- Blog/mysite/mysite/settings.py
+++ Blog/mysite/mysite/settings.py
@@ -116,3 +116,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure Django App for Heroku.
+try:
+    import django_heroku
+    django_heroku.settings(locals())
+except:
+    pass
